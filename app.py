38fa0c19b78e8fa3a8829d67ffc7ef5914429a00
kenenisaj7 <jaletokenenisa7@gmail.com>
2025-08-12 12:49:12 +0300

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from datetime import datetime
 from flask_cors import CORS
 import warnings
 warnings.filterwarnings('ignore')
+import os
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 # Configure logging
 logging.basicConfig(
